aee45783f2f2cf1cdb23986f36b5442a67d984be
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-29 20:34:23 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,18 @@
 module Memorable
 
-  def reset_all
-    self.all.clear
-  end
+  module ClassMethods
+    
+    def reset_all
+      self.all.clear
+    end
 
-  def count
-    self.all.count
-  end
+    def count
+      self.all.count
+    end
 
+  module InstanceMethods
+    
+    def initialize
+      
+    end
 end
